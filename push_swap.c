99a5a1037ef604db4ad27d63ea926f4005450cba
.frnki <frnki@frnki.dev>
2026-05-12 01:55:25 +0200

    add changes to push_swap.c

--- push_swap.c
+++ push_swap.c
@@ -9,10 +9,18 @@
 /*   Updated: 2026/04/20 16:20:42 by .frnki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "libft/libft.h"
 
-int	main()
+void	push_swap(int size, char **argv)
 {
-	ft_printf("test\n");
+	if (has_dupes(argv))
+		return ;
+	ft_printf("size: %i\n", size);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		return (1);
+	push_swap(--argc,argv);
 }
